be7d1834b561eb4d9cb2c7f3b4d66e683dcfcb61
andrewtreb <54001308+andrewtreb@users.noreply.github.com>
2022-12-04 00:20:55 -0600

    Revert "Adding Weather Data page"

--- lla/database.py
+++ lla/database.py
@@ -1,8 +1,6 @@
 import pymongo
 from bson.objectid import ObjectId
 import os
-from datetime import datetime, timedelta
-import pandas as pd
 
 class database:
     def __init__(self):
@@ -16,7 +14,6 @@ class database:
 
         self.db = self.client.limeLakeAnalytics
         self.posts = self.db.posts
-        self.weatherData = self.db.weatherData
 
 
     def get_posts(self):
@@ -24,16 +21,5 @@ class database:
 
     def get_post(self, id):
         return self.posts.find_one({"_id": ObjectId(id)})
-
-    def get_allWeatherData(self):
-        #lastHour = datetime.now() - timedelta(hours = 1)
-        #criteria = { 'data_time': {'$gt': lastHour}}
-
-        cursor = self.weatherData.find()
-        df = pd.DataFrame(list(cursor))
-
-        #print(df.shape)
-
-        return df
 
 
